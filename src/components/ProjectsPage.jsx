062813e842a3bec67ab008aa695459c759a2bd88
Rohan Virmani <rohanster@gmail.com>
2025-08-14 00:14:55 -0400

    updates projects and about us

--- src/components/ProjectsPage.jsx
+++ src/components/ProjectsPage.jsx
@@ -12,47 +12,29 @@ import '../styles/pageStyles.css'; // For specific page styling
 
 export default function ProjectsPage() {
   const projects = [
-    { id: 1, title: 'Ballerina', year: 2023, image: '/images/project1.jpg', description: 'A gripping drama about urban solitude.' },
-    { id: 2, title: 'Friendship', year: 2022, image: '/images/project2.jpg', description: 'Award-winning psychological thriller.' },
-    { id: 3, title: 'The Green Mile', year: 2021, image: '/images/project3.jpg', description: 'Documentary exploring sustainable living.' },
-    { id: 4, title: 'The Green Mile', year: 2021, image: '/images/project3.jpg', description: 'Documentary exploring sustainable living.' },
+    { id: 1, title: 'Small Wishes', year: 2023, image: '/images/Small-wishes-poster.png', description: 'A gripping drama about urban solitude.' },
+    { id: 2, title: 'Little Wings', year: 2022, image: '/images/project2.jpg', description: 'Award-winning psychological thriller.' },
+    {id: 3, title: 'Little Wings', year: 2022, image: '/images/project2.jpg', description: 'Award-winning psychological thriller.' },
+    {id: 3, title: 'Little Wings', year: 2022, image: '/images/project2.jpg', description: 'Award-winning psychological thriller.' },
+    {id: 3, title: 'Little Wings', year: 2022, image: '/images/project2.jpg', description: 'Award-winning psychological thriller.' },
+     
+ 
 
 
     // Add more projects
   ];
 
   return (
-    <div className="page-wrapper projects-page">
-      <div className="page-content">
-        <h2>Our Film Projects</h2>
-        <Swiper
-          modules={[Pagination, Navigation, Grid]}
-          grid={{
-          rows: 1,
-          fill: 'row',
-          }}
-          slidesPerView={4} // Show 2 images per row
-          spaceBetween={30}
-          navigation // Enable navigation for sub-carousel
-          pagination={{ clickable: true }} // Enable pagination for sub-carousel
-          
-          className="projects-swiper"
-          
-
-          style={{ width: '80%', height: '70%', maxWidth: '1200px' }} // Adjust size
-        >
+   <div className="page-wrapper projects-page"
+    style={{ backgroundImage: 'url(/images/CCA_BTS.jpg)' }}>
+  <div className="background-blur-overlay"></div>
+  <div className="page-content projects-page">
+      <h2>Our Film Projects</h2>
+      <div className="project-image-container"> {/* New container */}
           {projects.map((project) => (
-            <SwiperSlide key={project.id}>
-              <div className="project-card">
-                <img src={project.image} alt={project.title} className="project-image" />
-                <h3>{project.title}</h3>
-                {/* <p>{project.description}</p> */}
-                {/* Add a link to a dedicated project page if applicable */}
-              </div>
-            </SwiperSlide>
+              <img key={project.id} src={project.image} alt={project.title} className="project-image" />
           ))}
-        </Swiper>
       </div>
-    </div>
-  );
+  </div>
+</div> );
 }
--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -8,7 +8,7 @@ export default function WelcomePage() {
        <video className="background-video" autoPlay loop muted>
         <source src="/videos/VFP Home Page - Loop High quality.mp4" type="video/mp4" />
       </video>
-      <div className="background-blur-overlay"></div> {/* For the blur effect */}
+      {/* <div className="background-blur-overlay"></div> For the blur effect */}
       <div className="page-content welcome-page">
         <h1>Viewfinder Film Productions</h1>
         <p>Viewfinder Film Productions (VFP) Inc. is a marketing & production company dedicated to promoting the work of South Asian creators around the world.</p>
